63205595517941055ff19c2635f70a41f0a2a77e
Aleksandar Gotev <alex@gotev.net>
2019-02-02 18:30:21 +0100

    converted demo app tests to kotlin

---
+++ app/demo/src/androidTest/java/net/gotev/recycleradapterdemo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package net.gotev.recycleradapterdemo
+
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = ApplicationProvider.getApplicationContext<Application>()
+
+        assertEquals("net.gotev.recycleradapterdemo", appContext.packageName)
+    }
+}
