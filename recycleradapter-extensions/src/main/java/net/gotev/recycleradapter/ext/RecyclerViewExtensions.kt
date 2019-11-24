55504fb939ec69c4aaf9371395f637e75396527d
Alex Gotev <alex@gotev.net>
2019-11-24 13:26:44 +0100

    added shared view pool and convenience initializers for recycler view

--- recycleradapter-extensions/src/main/java/net/gotev/recycleradapter/ext/RecyclerViewExtensions.kt
+++ recycleradapter-extensions/src/main/java/net/gotev/recycleradapter/ext/RecyclerViewExtensions.kt
@@ -1,7 +1,12 @@
 package net.gotev.recycleradapter.ext
 
+import android.content.Context
 import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import net.gotev.recycleradapter.RecyclerAdapter
+
+val sharedViewPool by lazy { RecyclerView.RecycledViewPool() }
 
 /**
  * Applies swipe gesture detection on a RecyclerView.
@@ -10,9 +15,12 @@ import androidx.recyclerview.widget.RecyclerView
  */
 fun RecyclerView.applySwipeGesture(listener: SwipeListener) {
     ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-            0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
-        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
-                            target: RecyclerView.ViewHolder): Boolean {
+        0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
             return false
         }
 
@@ -20,4 +28,25 @@ fun RecyclerView.applySwipeGesture(listener: SwipeListener) {
             listener.onItemSwiped(viewHolder.adapterPosition, swipeDir)
         }
     }).attachToRecyclerView(this)
+}
+
+fun Context.prefetchingLinearLayoutManager(
+    initialPrefetch: Int = 5,
+    orientation: Int = RecyclerView.VERTICAL
+) = LinearLayoutManager(this, orientation, false).apply {
+    isItemPrefetchEnabled = true
+    initialPrefetchItemCount = initialPrefetch
+}
+
+fun RecyclerView.withSharedViewPool() = apply {
+    setRecycledViewPool(sharedViewPool)
+}
+
+fun RecyclerView.setupWithPrefetchingLinearLayoutAndSharedViewPool(
+    recyclerAdapter: RecyclerAdapter,
+    initialPrefetch: Int = 5
+) {
+    withSharedViewPool()
+    layoutManager = context.prefetchingLinearLayoutManager(initialPrefetch)
+    adapter = recyclerAdapter
 }
